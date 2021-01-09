745111d8ce528d8d8b08a408466fb925bb7fd9dc
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-01-08 22:53:26 -0500

    Update set.py

--- set.py
+++ set.py
@@ -6,26 +6,28 @@ write_key = os.environ.get('WRITE_KEY')    # GitHub action needs to set env vari
 mw = MicroWriter(write_key=write_key)
 assert mw.key_difficulty(mw.write_key)>=12, "You need a key of difficulty 12 to create a stream"
 
-PLAYERS = { 'Hikaru':'hikaru_nakamura',
-            'Firouzja2003':'alireza_firouzja',
-            'GMWSO':'wesley_so',
-            'LyonBeast':'maxime_vachier_lagrave',
-            'nihalsarin':'nihal_sarin',
-            'DanielNaroditsky':'daniel_naroditsky',
-            'penguingm1':'andrew_tang',
-            'Oleksander_Bortnyk':'oleksandr_bortnyk',
-            'PinIsMightier':'microprediction'}
-            
+PLAYERS = {'Hikaru': 'hikaru_nakamura',
+           'Firouzja2003': 'alireza_firouzja',
+           'GMWSO': 'wesley_so',
+           'LyonBeast': 'maxime_vachier_lagrave',
+           'nihalsarin': 'nihal_sarin',
+           'DanielNaroditsky': 'daniel_naroditsky',
+           'PinIsMightier': 'microprediction'}
 
 URL_TEMPLATE = 'https://api.chess.com/pub/player/PLAYER/stats'
-CATEGORIES = ['chess_blitz','chess_bullet']
+CATEGORIES = ['chess_blitz', 'chess_bullet']
 
-if __name__=='__main__':
-  # Chess.Com ratings 
-  for category in CATEGORIES:
-    for handle,player in PLAYERS.items():
-       url = URL_TEMPLATE.replace('PLAYER',player)
-       data = getjson(url)
-       value = data[category]['last']['rating']
-       name  = category+'_'+handle+'.json'
-       mw.set(name=name,value=value)
+if __name__ == '__main__':
+    # Chess.Com ratings
+    for category in CATEGORIES:
+        for handle, player in PLAYERS.items():
+            url = URL_TEMPLATE.replace('PLAYER', handle)
+            data = getjson(url)
+            try:
+                value = data[category]['last']['rating']
+                name = category + '_' + player + '.json'
+                print( (name, value, mw.set(name=name,value=value) ) )
+            except:
+                print(data)
+
+      
