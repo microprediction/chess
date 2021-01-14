74eb59d66b9a2bce12513378aad1610a87fd32fe
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-01-14 13:27:20 -0500

    Update set.py

--- set.py
+++ set.py
@@ -33,8 +33,8 @@ if __name__ == '__main__':
                     print( (level_name, current_value, mw.set(name=level_name,value=current_value) ) )
                 else:
                     if int(previous_value) != int(current_value):  
-                        # Somebody's been playing chess 
                         print( (level_name, current_value, mw.set(name=level_name,value=current_value) ) )
+                        print( level_name+' updated to '+str(current_value) )
                     else:
                         print( (level_name, current_value, 'unchanged') )
             else:
