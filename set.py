06a236500037756ef50cca8bc7eff1595fccb50b
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-01-26 10:07:37 -0500

    Update set.py

--- set.py
+++ set.py
@@ -1,42 +1,11 @@
 import os
 from getjson import getjson
 from microprediction import MicroWriter
+from config import ACTIVE, URL_TEMPLATE, CATEGORIES
 
 write_key = os.environ.get('WRITE_KEY')    # GitHub action needs to set env variable. You need to create a GitHub secret called WRITE_KEY
 mw = MicroWriter(write_key=write_key)
 assert mw.key_difficulty(mw.write_key)>=12, "You need a key of difficulty 12 to create a stream"
-
-# Active players
-ACTIVE = {'chess_blitz':{'Hikaru': 'hikaru_nakamura',
-                    'Firouzja2003': 'alireza_firouzja',
-                    'nihalsarin':'nihal_sarin',
-                    'DanielNaroditsky': 'daniel_naroditsky',
-                    'FarOctopus':'morgan_lu',
-                    'viswanath270':'viswanath thatha',
-                     'sshivaji':'shivkumar shivaji',
-                    'viswanath270':'viswanath thatha',
-                          'caldyc':'chris calderwood',
-                          'swamijs':'swamijs',
-                         '9epoch':'nathan szeitli',
-                         'erik':'erick allebest',    # Founder of chess.com 
-                         'Trilobe8','nik sykiotis'
-                  },
-          'chess_bullet':{'Hikaru': 'hikaru_nakamura',
-                   'Firouzja2003': 'alireza_firouzja',
-                   'DanielNaroditsky': 'daniel_naroditsky',
-                   'penguingm1':'andrew_tang',
-                    'wonderfultime':'tuan_minh_le',
-                    'PinIsMightier':'peter_cotton',
-                    'FarOctopus':'morgan_lu',
-                     'sshivaji':'shivkumar shivaji',
-                      '9epoch':'nathan szeitli',
-                          'erik':'erick allebest',
-                          'Trilobe8','nik sykiotis'
-                   }
-            }
-
-URL_TEMPLATE = 'https://api.chess.com/pub/player/HANDLE/stats'
-CATEGORIES = ['chess_blitz', 'chess_bullet']
 
 if __name__ == '__main__':
     for category in CATEGORIES:
