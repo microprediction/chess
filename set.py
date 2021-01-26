65f7611840cb56f514b663a48509091bd6bb3118
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-01-26 11:22:57 -0500

    Update set.py

--- set.py
+++ set.py
@@ -4,28 +4,32 @@ from getjson import getjson
 from config import ACTIVE, URL_TEMPLATE, mw
 
 if __name__ == '__main__':
-    for category in ACTIVE.keys():
-        for handle, player in ACTIVE[category].items():
-            url = URL_TEMPLATE.replace('HANDLE', handle.lower())
-            data = getjson(url)
-            if data is not None:
-                if data.get(category):
-                    current_value = int(data[category]['last']['rating'])
-                    level_name = category + '_level_'  + handle + '.json'         # Name of stream with rating level
-                    previous_value = mw.get_current_value(name=level_name)
-                    if previous_value is None:
-                        print('No previous value for '+level_name)
-                        print( mw.set(name=level_name,value=current_value) )
-                    else:
-                        if int(float(previous_value)) != int(float(current_value)):  
-                            print( mw.set(name=level_name,value=current_value) ) 
-                            print( level_name+' updated to '+str(current_value) )
+    for _ in range(60):
+        start_time = time.time()
+        for category in ACTIVE.keys():
+            for handle, player in ACTIVE[category].items():
+                url = URL_TEMPLATE.replace('HANDLE', handle.lower())
+                data = getjson(url)
+                if data is not None:
+                    if data.get(category):
+                        current_value = int(data[category]['last']['rating'])
+                        level_name = category + '_level_'  + handle + '.json'         # Name of stream with rating level
+                        change_name = category + '_change_'  + handle + '.json'       # Name of stream with rating changes
+                        previous_value = mw.get_current_value(name=level_name)
+                        if previous_value is None:
+                            mw.set(name=level_name,value=current_value)
+                            mw.set(name=change_name,value=0)
                         else:
-                            print( (level_name, current_value, ' is unchanged') )
-                            print( mw.touch(name=level_name) )
-                else:
-                    print(player+ ' has never played '+category)
-            else:
-                print(url)
-           
-      
+                            if int(float(previous_value)) != int(float(current_value)):
+                                mw.set(name=level_name,value=current_value)
+                                mw.set(name=change_name,value=current_value-previous_value)
+                                print( level_name+' updated to '+str(current_value) )
+                            else:
+                                mw.touch(name=level_name)
+                                mw.touch(name=change_name)
+        elapsed = time.time()-start_time
+        print(str(elapsed))
+        if elapsed>0:
+            time.sleep(60.-elapsed)
+        else:
+            print('Running behind',flush=True)
