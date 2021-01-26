2e79ed9256d09bf3a0f21dd7f5f6ee008696a3f1
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-01-26 10:16:37 -0500

    Update daily.py

--- daily.py
+++ daily.py
@@ -1,26 +1,11 @@
 import os
 from getjson import getjson
 from microprediction import MicroWriter
-
-write_key = os.environ.get('WRITE_KEY')    # GitHub action needs to set env variable. You need to create a GitHub secret called WRITE_KEY
-mw = MicroWriter(write_key=write_key)
-assert mw.key_difficulty(mw.write_key)>=12, "You need a key of difficulty 12 to create a stream"
-
-PLAYERS = {'Hikaru': 'hikaru_nakamura',
-           'Firouzja2003': 'alireza_firouzja',
-           'GMWSO': 'wesley_so',
-           'LyonBeast': 'maxime_vachier_lagrave',
-           'nihalsarin': 'nihal_sarin',
-           'DanielNaroditsky': 'daniel_naroditsky',
-           'PinIsMightier': 'halloween_gambit'}
-
-URL_TEMPLATE = 'https://api.chess.com/pub/player/HANDLE/stats'
-CATEGORIES = ['chess_blitz', 'chess_bullet']
+from config import ACTIVE, mw
 
 if __name__ == '__main__':
-    # Chess.Com ratings
     for category in CATEGORIES:
-        for handle, player in PLAYERS.items():
+        for handle, player in ACTIVE.items():
             url = URL_TEMPLATE.replace('HANDLE', handle)
             data = getjson(url)
             try:
