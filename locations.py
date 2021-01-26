4e1991b5468077c4c14d6b8b42d83716a80f5979
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-01-26 12:16:00 -0500

    Update locations.py

--- locations.py
+++ locations.py
@@ -1,15 +1,18 @@
 from config import ACTIVE, URL_TEMPLATE, STREAM_URL_TEMPLATE, STREAM_TYPES, ANALYSIS_DIR, stream_url
 import os
 import json
+from pprint import pprint
 
 def dump_summary():
+  CATEGORIES = ACTIVE.keys()
   for category in ACTIVE.keys():
       for handle, player in ACTIVE[category].items():
            summary = {'urls':{'stats':URL_TEMPLATE.replace('HANDLE', handle.lower() ),
-                              category+'_'+'level': stream_url(category=category,handle=handle, stream_type='level'),
-                              category+'_'+'change':stream_url(category=category,handle=handle, stream_type='change'),
-                              category+'_'+'daily':stream_url(category=category,handle=handle, stream_type='daily')}}
-           directory = ANALYSIS_DIR+os.path.sep+handle.lower()
+                              'level': stream_url(category=category,handle=handle, stream_type='level'),
+                              'change':stream_url(category=category,handle=handle, stream_type='change'),
+                              'daily':stream_url(category=category,handle=handle, stream_type='daily')}}
+           pprint(summary)
+           directory = ANALYSIS_DIR+os.path.sep+handle.lower()+os.path.sep+category
            summary_file = directory+os.path.sep+'summary.json'
            try:
               os.mkdirs(directory)
