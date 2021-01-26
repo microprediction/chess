26e299c63c8b2886befd8dd2c64d19236104973d
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-01-26 11:43:36 -0500

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,13 +1,8 @@
 # Personalized analysis... more to come
-from config import ACTIVE, URL_TEMPLATE
+from config import ACTIVE, URL_TEMPLATE, STREAM_URL_TEMPLATE, STREAM_TYPES, ANALYSIS_DIR, stream_url
 import os
 
-STREAM_URL_TEMPLATE = 'https://www.microprediction.org/stream_dashboard.html?stream=chess_CATEGORY_TYPE_HANDLE
-TYPES = ['daily','level','change']
-ANALYSIS = 'analysis'
 
-def stream_url(category,stream_type,handle):
-     return STEAM_URL_TEMPLATE.replace('CATEGORY',category).replace('TYPE',stream_type).replace('HANDLE',handle)
 
 def summary_dump():
   for category in ACTIVE.keys():
@@ -16,7 +11,7 @@ def summary_dump():
                     'level': stream_url(category=category,handle=handle, stream_type='level'),
                     'change':stream_url(category=category,handle=handle, stream_type='level'),
                     'daily':stream_url(category=category,handle=handle, stream_type='daily')}}
-           directory = ANALYSIS+os.path.sep+handle.lower()
+           directory = ANALYSIS_DIR+os.path.sep+handle.lower()
            summary_file = directory+os.path.sep+'summary.json'
            try:
               os.mkdirs(directory)
